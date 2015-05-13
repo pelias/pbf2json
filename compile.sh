d4862228700cfbe5f94727a50a18fddac3376a2d
Peter Johnson <insomnia@rcpt.at>
2015-05-13 12:39:58 +0200

    commit binaries for common architectures

--- compile.sh
+++ compile.sh
@@ -12,57 +12,57 @@ go-linux-arm build pbf2json.go;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.linux-arm;
 
-echo "[compile] linux i386";
-go-linux-386 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.linux-ia32;
+# echo "[compile] linux i386";
+# go-linux-386 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.linux-ia32;
 
 echo "[compile] linux x64";
 go-linux-amd64 build pbf2json.go;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.linux-x64;
 
-echo "[compile] darwin i386";
-go-darwin-386 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.darwin-ia32;
+# echo "[compile] darwin i386";
+# go-darwin-386 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.darwin-ia32;
 
 echo "[compile] darwin x64";
 go-darwin-amd64 build pbf2json.go;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.darwin-x64;
 
-echo "[compile] windows i386";
-go-windows-386 build pbf2json.go;
-chmod +x pbf2json.exe;
-mv pbf2json.exe build/pbf2json.win32-ia32;
+# echo "[compile] windows i386";
+# go-windows-386 build pbf2json.go;
+# chmod +x pbf2json.exe;
+# mv pbf2json.exe build/pbf2json.win32-ia32;
 
 echo "[compile] windows x64";
 go-windows-amd64 build pbf2json.go;
 chmod +x pbf2json.exe;
 mv pbf2json.exe build/pbf2json.win32-x64;
 
-echo "[compile] freebsd arm";
-go-freebsd-arm build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.freebsd-arm;
+# echo "[compile] freebsd arm";
+# go-freebsd-arm build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.freebsd-arm;
 
-echo "[compile] freebsd i386";
-go-freebsd-386 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.freebsd-ia32;
+# echo "[compile] freebsd i386";
+# go-freebsd-386 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.freebsd-ia32;
 
-echo "[compile] freebsd x64";
-go-freebsd-amd64 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.freebsd-x64;
+# echo "[compile] freebsd x64";
+# go-freebsd-amd64 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.freebsd-x64;
 
-echo "[compile] openbsd i386";
-go-openbsd-386 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.openbsd-ia32;
+# echo "[compile] openbsd i386";
+# go-openbsd-386 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.openbsd-ia32;
 
-echo "[compile] openbsd x64";
-go-openbsd-amd64 build pbf2json.go;
-chmod +x pbf2json;
-mv pbf2json build/pbf2json.openbsd-x64;
+# echo "[compile] openbsd x64";
+# go-openbsd-amd64 build pbf2json.go;
+# chmod +x pbf2json;
+# mv pbf2json build/pbf2json.openbsd-x64;
