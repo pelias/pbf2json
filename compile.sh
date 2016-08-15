1dd31e0c39c460766261db33fd0606054904d6a5
missinglink <insomnia@rcpt.at>
2016-08-15 15:01:23 +0200

    fix typo

--- compile.sh
+++ compile.sh
@@ -13,73 +13,73 @@ function assert() {
 }
 
 echo "[compile] linux arm";
-env GOOD=linux GOARCH=arm go build;
+env GOOS=linux GOARCH=arm go build;
 assert $?;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.linux-arm;
 
 # echo "[compile] linux i386";
-# env GOOD=linux GOARCH=386 go build;
+# env GOOS=linux GOARCH=386 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.linux-ia32;
 
 echo "[compile] linux x64";
-env GOOD=linux GOARCH=amd64 go build;
+env GOOS=linux GOARCH=amd64 go build;
 assert $?;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.linux-x64;
 
 # echo "[compile] darwin i386";
-# env GOOD=darwin GOARCH=386 go build;
+# env GOOS=darwin GOARCH=386 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.darwin-ia32;
 
 echo "[compile] darwin x64";
-env GOOD=darwin GOARCH=amd64 go build;
+env GOOS=darwin GOARCH=amd64 go build;
 assert $?;
 chmod +x pbf2json;
 mv pbf2json build/pbf2json.darwin-x64;
 
 # echo "[compile] windows i386";
-# env GOOD=windows GOARCH=386 go build;
+# env GOOS=windows GOARCH=386 go build;
 # assert $?;
 # chmod +x pbf2json.exe;
 # mv pbf2json.exe build/pbf2json.win32-ia32;
 
 echo "[compile] windows x64";
-env GOOD=windows GOARCH=amd64 go build -o pbf2json.exe;
+env GOOS=windows GOARCH=386 go build -o pbf2json.exe;
 assert $?;
 chmod +x pbf2json.exe;
 mv pbf2json.exe build/pbf2json.win32-x64;
 
 # echo "[compile] freebsd arm";
-# env GOOD=freebsd GOARCH=arm go build;
+# env GOOS=freebsd GOARCH=arm go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.freebsd-arm;
 
 # echo "[compile] freebsd i386";
-# env GOOD=freebsd GOARCH=386 go build;
+# env GOOS=freebsd GOARCH=386 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.freebsd-ia32;
 
 # echo "[compile] freebsd x64";
-# env GOOD=freebsd GOARCH=amd64 go build;
+# env GOOS=freebsd GOARCH=amd64 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.freebsd-x64;
 
 # echo "[compile] openbsd i386";
-# env GOOD=openbsd GOARCH=386 go build;
+# env GOOS=openbsd GOARCH=386 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.openbsd-ia32;
 
 # echo "[compile] openbsd x64";
-# env GOOD=openbsd GOARCH=amd64 go build;
+# env GOOS=openbsd GOARCH=amd64 go build;
 # assert $?;
 # chmod +x pbf2json;
 # mv pbf2json build/pbf2json.openbsd-x64;
