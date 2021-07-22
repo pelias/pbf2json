213adc160a7084b3793011de0e62daff7029817f
Timon Masberg <kontakt@timonmasberg.de>
2021-07-22 16:48:17 +0200

    feat(relation-outer-members-centroid): create util functions for misc operations in util.go

---
+++ util.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	geo "github.com/paulmach/go.geo"
+	"math"
+	"strconv"
+)
+
+func IsPointSetClosed(points *geo.PointSet) bool {
+	return points.First().Equals(points.Last())
+}
+
+func PointToLatLon(point *geo.Point) map[string]string {
+	var latLon = make(map[string]string)
+	latLon["lat"] = strconv.FormatFloat(point.Lat(), 'f', 7, 64)
+	latLon["lon"] = strconv.FormatFloat(point.Lng(), 'f', 7, 64)
+
+	return latLon
+}
+
+func GetAreaOfBounds(bound *geo.Bound) float64 {
+	return math.Max(bound.GeoWidth(), 0.000001) * math.Max(bound.GeoHeight(), 0.000001)
+}
